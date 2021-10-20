74b8fd7a1dc3ca081dfd3a39b4120439b11f1693
TSUJIMARU-PC\Norie Tsujimaru <tsujimaru@satoh-er.com>
2021-10-20 15:08:09 +0900

    座標を格納する変数名を変更（coordinates -> points）

--- transmission_rate_base.py
+++ transmission_rate_base.py
@@ -76,11 +76,11 @@ def base_transmission_rate_circle(radius: float, distance_vertical: float, dista
     return rate
 
 
-def base_transmission_rate_triangle(coordinates: dict, distance_vertical: float, distance_horizontal: float) -> float:
+def base_transmission_rate_triangle(points: dict, distance_vertical: float, distance_horizontal: float) -> float:
     """
     三角形の花ブロックの基準透過率を計算する
 
-    :param coordinates: 手前側の三角形ABCの各頂点の座標(x, y)
+    :param points: 手前側の三角形ABCの各頂点の座標(x, y)
     :param distance_vertical: 点の影の垂直方向の移動距離[mm]
     :param distance_horizontal: 点の影の水平方向の移動距離[mm]
     :return:三角形の花ブロックの基準透過率[-]
@@ -91,20 +91,17 @@ def base_transmission_rate_triangle(coordinates: dict, distance_vertical: float,
         rate = 0.0
     else:
         # 奥側の三角形A'B'C'の各頂点の座標を設定
-        coordinates['peak_a_dash'] = (coordinates['peak_a'][0] + distance_vertical,
-                                      coordinates['peak_a'][1] + distance_horizontal)
-        coordinates['peak_b_dash'] = (coordinates['peak_b'][0] + distance_vertical,
-                                      coordinates['peak_b'][1] + distance_horizontal)
-        coordinates['peak_c_dash'] = (coordinates['peak_c'][0] + distance_vertical,
-                                      coordinates['peak_c'][1] + distance_horizontal)
+        points['peak_a_dash'] = (points['peak_a'][0] + distance_vertical, points['peak_a'][1] + distance_horizontal)
+        points['peak_b_dash'] = (points['peak_b'][0] + distance_vertical, points['peak_b'][1] + distance_horizontal)
+        points['peak_c_dash'] = (points['peak_c'][0] + distance_vertical, points['peak_c'][1] + distance_horizontal)
 
         # 三角形の内側にある頂点を判定
-        peak_check_results = get_witch_peak_inside(coordinates)
+        peak_check_results = get_witch_peak_inside(points)
 
         # 透過率を計算
         if True in peak_check_results.values():
             # 基準点、内部点の高さを取得
-            h, h_dash = get_point_heights(peak_check_results, coordinates)
+            h, h_dash = get_point_heights(peak_check_results, points)
             # 透過率を計算
             rate = (h_dash / h) ** 2
         else:
@@ -114,12 +111,12 @@ def base_transmission_rate_triangle(coordinates: dict, distance_vertical: float,
     return rate
 
 
-def get_witch_peak_inside(coordinates: dict) -> dict:
+def get_witch_peak_inside(points: dict) -> dict:
 
     """
     三角形の内側にある頂点を判定する
 
-    :param coordinates: 手前側の三角形ABC、奥側の三角形ABCのの各頂点の座標(x, y)
+    :param points: 手前側の三角形ABC、奥側の三角形ABCのの各頂点の座標(x, y)
     :return:各頂点の判定結果
     """
 
@@ -128,42 +125,42 @@ def get_witch_peak_inside(coordinates: dict) -> dict:
     matrix_const = np.zeros(2)
 
     # 行列に値を設定（係数部分）
-    matrix_coeff[0][0] = coordinates['peak_b'][0] - coordinates['peak_a'][0]
-    matrix_coeff[0][1] = coordinates['peak_c'][0] - coordinates['peak_a'][0]
-    matrix_coeff[1][0] = coordinates['peak_b'][1] - coordinates['peak_a'][1]
-    matrix_coeff[1][1] = coordinates['peak_c'][1] - coordinates['peak_a'][1]
+    matrix_coeff[0][0] = points['peak_b'][0] - points['peak_a'][0]
+    matrix_coeff[0][1] = points['peak_c'][0] - points['peak_a'][0]
+    matrix_coeff[1][0] = points['peak_b'][1] - points['peak_a'][1]
+    matrix_coeff[1][1] = points['peak_c'][1] - points['peak_a'][1]
 
     # 判定結果を格納するディクショナリを用意
     results = {}
 
     # 頂点Aの判定
-    matrix_const[0] = coordinates['peak_a'][0] - coordinates['peak_a_dash'][0]
-    matrix_const[1] = coordinates['peak_a'][1] - coordinates['peak_a_dash'][1]
+    matrix_const[0] = points['peak_a'][0] - points['peak_a_dash'][0]
+    matrix_const[1] = points['peak_a'][1] - points['peak_a_dash'][1]
     results['peak_a'] = judge_is_peak_inside(matrix_coeff, matrix_const)
 
     # 頂点Bの判定
-    matrix_const[0] = coordinates['peak_b'][0] - coordinates['peak_a_dash'][0]
-    matrix_const[1] = coordinates['peak_b'][1] - coordinates['peak_a_dash'][1]
+    matrix_const[0] = points['peak_b'][0] - points['peak_a_dash'][0]
+    matrix_const[1] = points['peak_b'][1] - points['peak_a_dash'][1]
     results['peak_b'] = judge_is_peak_inside(matrix_coeff, matrix_const)
 
     # 頂点Cの判定
-    matrix_const[0] = coordinates['peak_c'][0] - coordinates['peak_a_dash'][0]
-    matrix_const[1] = coordinates['peak_c'][1] - coordinates['peak_a_dash'][1]
+    matrix_const[0] = points['peak_c'][0] - points['peak_a_dash'][0]
+    matrix_const[1] = points['peak_c'][1] - points['peak_a_dash'][1]
     results['peak_c'] = judge_is_peak_inside(matrix_coeff, matrix_const)
 
     # 頂点A'の判定
-    matrix_const[0] = coordinates['peak_a_dash'][0] - coordinates['peak_a'][0]
-    matrix_const[1] = coordinates['peak_a_dash'][1] - coordinates['peak_a'][1]
+    matrix_const[0] = points['peak_a_dash'][0] - points['peak_a'][0]
+    matrix_const[1] = points['peak_a_dash'][1] - points['peak_a'][1]
     results['peak_a_dash'] = judge_is_peak_inside(matrix_coeff, matrix_const)
 
     # 頂点B'の判定
-    matrix_const[0] = coordinates['peak_b_dash'][0] - coordinates['peak_a'][0]
-    matrix_const[1] = coordinates['peak_b_dash'][1] - coordinates['peak_a'][1]
+    matrix_const[0] = points['peak_b_dash'][0] - points['peak_a'][0]
+    matrix_const[1] = points['peak_b_dash'][1] - points['peak_a'][1]
     results['peak_b_dash'] = judge_is_peak_inside(matrix_coeff, matrix_const)
 
     # 頂点C'の判定
-    matrix_const[0] = coordinates['peak_c_dash'][0] - coordinates['peak_a'][0]
-    matrix_const[1] = coordinates['peak_c_dash'][1] - coordinates['peak_a'][1]
+    matrix_const[0] = points['peak_c_dash'][0] - points['peak_a'][0]
+    matrix_const[1] = points['peak_c_dash'][1] - points['peak_a'][1]
     results['peak_c_dash'] = judge_is_peak_inside(matrix_coeff, matrix_const)
 
     return results
@@ -193,24 +190,24 @@ def judge_is_peak_inside(matrix_coeff: np.zeros(shape=(2, 2)), matrix_const: np.
     return judge
 
 
-def get_point_heights(peak_check_results: dict, coordinates: dict) -> [float, float]:
+def get_point_heights(peak_check_results: dict, points: dict) -> [float, float]:
     """
     対辺からの基準点、内部点の高さを計算する
 
     :param peak_check_results: 各頂点が内側にあるかどうかの判定結果
-    :param coordinates: 手前側の三角形ABC、奥側の三角形ABCの各頂点の座標(x, y)
+    :param points: 手前側の三角形ABC、奥側の三角形ABCの各頂点の座標(x, y)
     :return:対辺からの基準点、内部点の高さ(mm)
     """
 
     # 内側にある頂点の座標を設定
     inside_peak_name = [key for key, value in peak_check_results.items() if value is True][0]
-    inside_point = coordinates[inside_peak_name]
+    inside_point = points[inside_peak_name]
 
     # 対象となる基準点、対辺の2点の座標を取得
     target = get_target_base_point_and_side(inside_peak_name)
-    base_point = coordinates[target['base_point']]
-    side_point1 = coordinates[target['side_point1']]
-    side_point2 = coordinates[target['side_point2']]
+    base_point = points[target['base_point']]
+    side_point1 = points[target['side_point1']]
+    side_point2 = points[target['side_point2']]
 
     # 対辺の2点を結ぶ一次方程式の係数を取得
     p, q, r = get_linear_equation(side_point1, side_point2)
@@ -262,18 +259,18 @@ def get_target_base_point_and_side(inside_peak_name: str) -> dict:
     return target
 
 
-def get_linear_equation(coordinates_point1: tuple, coordinates_point2: tuple) -> [float, float, float]:
+def get_linear_equation(points_01: tuple, points_02: tuple) -> [float, float, float]:
     """
         2点を通る一次方程式（px+qy+r=0）の各係数p,q,rを計算する
 
-        :param coordinates_point1: 1つ目の点の座標(x, y)
-        :param coordinates_point1: 2つ目の点の座標(x, y)
+        :param points_01: 1つ目の点の座標(x, y)
+        :param points_02: 2つ目の点の座標(x, y)
         :return:一次方程式（px+qy+r=0）の各係数p,q,r
     """
-    p = (coordinates_point2[1] - coordinates_point1[1]) / (coordinates_point2[0] - coordinates_point1[0])
+    p = (points_02[1] - points_01[1]) / (points_02[0] - points_01[0])
     q = -1.0
-    r = (coordinates_point2[0] * coordinates_point1[1] - coordinates_point1[0] * coordinates_point2[1])\
-        / (coordinates_point2[0] - coordinates_point1[0])
+    r = (points_02[0] * points_01[1] - points_01[0] * points_02[1])\
+        / (points_02[0] - points_01[0])
 
     return p, q, r
 
@@ -294,8 +291,8 @@ def get_point_height_from_line(point_coordinate: tuple, p: float, q: float, r: f
 
 def case_study_triangle():
 
-    triangle_coordinates = {'peak_a': (0, 0), 'peak_b': (0, 130), 'peak_c': (130, 0)}
-    print(base_transmission_rate_triangle(triangle_coordinates, 10, -50))
+    triangle_points = {'peak_a': (0, 0), 'peak_b': (0, 130), 'peak_c': (130, 0)}
+    print(base_transmission_rate_triangle(triangle_points, 10, -50))
 
 
 if __name__ == '__main__':
