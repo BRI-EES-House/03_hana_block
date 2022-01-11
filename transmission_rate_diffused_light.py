9ab1f4314e4e7fa2d3db509d0eb4b37335bda826
TSUJIMARU-PC\Norie Tsujimaru <tsujimaru@satoh-er.com>
2022-01-11 16:25:47 +0900

    太陽高度、太陽方位角の総当たりの組み合わせを作成（太陽方位角は-90度～90度の範囲に限定）、 天空光、地物反射光によって太陽高度の条件を変更（天空光は0度～90度、地物反射光は-90度～0度）

--- transmission_rate_diffused_light.py
+++ transmission_rate_diffused_light.py
@@ -8,27 +8,31 @@ import distance_point_shadow
 import transmission_rate_base
 
 
-def diffused_light_transmission_rate(spec: common.HanaBlockSpec) -> float:
+def diffused_light_transmission_rate(calc_target: str, spec: common.HanaBlockSpec) -> float:
     """
     四角形の花ブロックの拡散光の透過率を計算する
+    (花ブロックの方位は南向きとする）
 
+    :param calc_target: 計算対象（sky: 天空光、reflected: 反射光）
     :param spec:   花ブロックの仕様
     :return:四角形の花ブロックの拡散光の透過率[-]
     """
 
-    # 各乱数の総当たりの組み合わせを設定
-    random_numbers_cases = get_random_number_list()
+    # 太陽高度、太陽方位角の総当たりの組み合わせを設定
+    random_angles = get_random_angles_list(calc_target)
 
     # 結果格納用の配列を用意
     rate_s = []         # 透過率
     sun_altitudes = []   # 太陽高度
     sun_azimuth_angles = []  # 太陽方位角
 
-    for index, case in enumerate(random_numbers_cases):
+    for index, case in enumerate(random_angles):
 
         # 太陽高度、太陽方位角を計算
-        sun_altitude = get_random_sun_altitude(case[0])
-        sun_azimuth_angle = get_random_sun_azimuth_angle(case[1])
+        sun_altitude = case[0]
+        sun_azimuth_angle = case[1]
+        # sun_altitude = get_random_sun_altitude(case[0])
+        # sun_azimuth_angle = get_random_sun_azimuth_angle(case[1])
 
         # 点の影の垂直方向、水平方向の移動距離を計算
         distance_vertical, distance_horizontal = distance_point_shadow.distance_of_points_shadow(
@@ -56,20 +60,20 @@ def diffused_light_transmission_rate(spec: common.HanaBlockSpec) -> float:
         sun_azimuth_angles.append(sun_azimuth_angle)
 
     # デバッグ用
-    # # 計算結果をDataFrameに追加
-    # df = pd.DataFrame({'sun_altitude':sun_altitudes, 'sun_azimuth_angles': sun_azimuth_angles, 'rate_s': rate_s})
-    #
-    # # CSVファイルに出力
-    # df.to_csv('result/diffused_light_' + spec.type + '.csv')
+    # 計算結果をDataFrameに追加
+    df = pd.DataFrame({'sun_altitude': sun_altitudes, 'sun_azimuth_angles': sun_azimuth_angles, 'rate_s': rate_s})
+
+    # CSVファイルに出力
+    df.to_csv('result/diffused_light_' + calc_target + '_' + spec.type + '.csv')
 
     return statistics.mean(rate_s)
 
 
-def get_random_number_list() -> list:
+def get_random_angles_list(calc_target: str) -> list:
     """
-    0～1の範囲の乱数の総当たりの組み合わせを設定する
+    0～1の範囲の乱数の総当たりの組み合わせを設定し、太陽高度、太陽方位角のリストとして返す
 
-    :param なし
+    :param calc_target: 計算対象（sky: 天空光、reflected: 反射光）
     :return:乱数の総当たりの組み合わせ
     """
 
@@ -82,10 +86,25 @@ def get_random_number_list() -> list:
     # 太陽方位角を設定するための乱数を設定
     r_a = np.arange(0, 1 + r_step, r_step, dtype=float)
 
-    # 各乱数の総当たりの組み合わせを設定
-    random_numbers = list(itertools.product(r_h, r_a))
+    # 太陽高度を計算
+    if calc_target == 'sky':
+        sun_altitudes = np.array([get_random_sun_altitude(x) for x in r_h])
+    elif calc_target == 'reflected':
+        sun_altitudes = np.array([-1.0 * get_random_sun_altitude(x) for x in r_h])
+    else:
+        raise ValueError('計算対象「' + calc_target + '」は対象外です')
 
-    return random_numbers
+    # 太陽方位角を計算
+    sun_azimuth_angles_base = np.array([get_random_sun_azimuth_angle(x) for x in r_a])
+
+    # 太陽方位角を-90度～90度の範囲に限定 values[np.where((values>2) & (values<4))]
+    sun_azimuth_angles = sun_azimuth_angles_base[np.where((sun_azimuth_angles_base >= -90.0) &
+                                                 (sun_azimuth_angles_base <= 90.0))]
+
+    # 太陽高度、太陽方位角の総当たりの組み合わせを設定
+    random_angles = list(itertools.product(sun_altitudes, sun_azimuth_angles))
+
+    return random_angles
 
 
 def get_random_sun_altitude(random_number: float) -> float:
@@ -105,36 +124,60 @@ def get_random_sun_azimuth_angle(random_number: float) -> float:
     :param random_number: 0～1の乱数
     :return:任意の太陽方位角[degree]
     """
-    return math.degrees(2.0 * math.pi * random_number)
+
+    # 方位角を計算
+    angle = math.degrees(2.0 * math.pi * random_number)
+
+    # 南0度、西90度、北180度、東-90度に換算
+    if angle > 180.0:
+        angle = angle - 360.0
+
+    return angle
 
 
 def case_study():
 
     # 四角形の場合
     spec = common.HanaBlockSpec(
-        type='square', depth=100, inclination_angle=90, azimuth_angle=0, width=136.0, height=136.0)
-    print(spec.type + ':')
-    print(diffused_light_transmission_rate(spec))
+        type='square', depth=100, inclination_angle=90, azimuth_angle=0, width=136.0, height=136.0,
+        front_width=190.0, front_height=190.0
+    )
+    print(spec.type + '　sky:')
+    print(diffused_light_transmission_rate('sky', spec))
+    print(spec.type + '　reflected:')
+    print(diffused_light_transmission_rate('reflected', spec))
 
     # 円形の場合
     spec = common.HanaBlockSpec(
-        type='circle', depth=100, inclination_angle=90, azimuth_angle=0, radius=136.0 / 2.0)
-    print(spec.type + ':')
-    print(diffused_light_transmission_rate(spec))
+        type='circle', depth=100, inclination_angle=90, azimuth_angle=0, radius=136.0 / 2.0,
+        front_width=190.0, front_height=190.0
+    )
+    print(spec.type + '　sky:')
+    print(diffused_light_transmission_rate('sky', spec))
+    print(spec.type + '　reflected:')
+    print(diffused_light_transmission_rate('reflected', spec))
 
     # 三角形の場合（その1）
     spec = common.HanaBlockSpec(
         type='triangle', depth=100, inclination_angle=90, azimuth_angle=0,
-        points={'peak_a': (0, 0), 'peak_b': (0, 130), 'peak_c': (130, 130)})
-    print(spec.type + ':')
-    print(diffused_light_transmission_rate(spec))
+        points={'peak_a': (0, 0), 'peak_b': (0, 130), 'peak_c': (130, 130)},
+        front_width=190.0, front_height=190.0
+    )
+    print(spec.type + '　sky:')
+    print(diffused_light_transmission_rate('sky', spec))
+    print(spec.type + '　reflected:')
+    print(diffused_light_transmission_rate('reflected', spec))
 
     # 三角形の場合（その2）
     spec = common.HanaBlockSpec(
         type='triangle', depth=150, inclination_angle=90, azimuth_angle=0,
-        points={'peak_a': (0, 0), 'peak_b': (130, 130), 'peak_c': (0, 130)})
-    print(spec.type + ':')
-    print(diffused_light_transmission_rate(spec))
+        points={'peak_a': (0, 0), 'peak_b': (130, 130), 'peak_c': (0, 130)},
+        front_width=190.0, front_height=190.0
+    )
+    print(spec.type + '　sky:')
+    print(diffused_light_transmission_rate('sky', spec))
+    print(spec.type + '　reflected:')
+    print(diffused_light_transmission_rate('reflected', spec))
 
 
 if __name__ == '__main__':
